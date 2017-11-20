488ede7006485caf974c9647f829251b60378474
Nischay Pro <NischayPro@yahoo.co.in>
2017-11-21 05:11:21 +0530

    code: slight tweaks

--- vad.py
+++ vad.py
@@ -8,10 +8,10 @@ class VoiceActivityDetector():
     
     def __init__(self, wave_input_filename):
         self._read_wav(wave_input_filename)._convert_to_mono()
-        self.sample_window = 0.5 #20 ms
-        self.sample_overlap = 0.01 #10ms
+        self.sample_window = 1 #20 ms
+        self.sample_overlap = 0.1 #10ms
         self.speech_window = 3 #half a second
-        self.speech_energy_threshold = 0.6 #60% of energy in voice band
+        self.speech_energy_threshold = 0.3 #60% of energy in voice band
         self.speech_start_band = 300
         self.speech_end_band = 3000
            
